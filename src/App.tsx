415b745b5bbfbc7dd671fb704601744fbdf7803c
Salsabeel Deremah <salsasira@gmail.com>
2026-09-23 21:48:46 +0700

    เพิ่มหน้า summary
    This reverts commit 44935a2b288d26576a06963972acbf2db74ea789.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import Summary from './screens/Summary'
+import SmallStep from './screens/SmallStep'
 import './App.css'
 
 export default function App() {
   // Step state and navigation will be added when the flow is implemented.
-  // previewState can be 'mixed' | 'actionable' | 'uncontrollable'
-  return <Summary previewState="mixed" />
+  return <SmallStep />
 }
-
 
 
 
